dd2dae4648004b511e340d0113757afe4740ebbe
Alicia Barros <alicia.jean90@gmail.com>
2023-04-15 22:13:18 -0400

    worked on clickCoffee functions & coffeView

--- script.js
+++ script.js
@@ -13,13 +13,20 @@ const data = window.data;
 
 function updateCoffeeView(coffeeQty) {
   // Create a reference to the element who's ID is 'coffee_counter'
+  const coffeeCounter = document.getElementById("coffee_counter");
+
   // Set the innerText of that element to be the coffeeQty passed into this function
+  coffeeCounter.innerText = coffeeQty;
 }
 
 function clickCoffee(data) {
   // Increment the data object's (passed into this function) coffee property by one
+  data.coffee += 1;
+
   // call the updateCoffeeView function and pass it the newly updated data.coffee property
+  updateCoffeeView(data.coffee);
   // call the renderProducers function and pass it the data object
+  renderProducers(data);
 }
 
 /**************
